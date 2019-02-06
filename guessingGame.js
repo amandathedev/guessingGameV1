505586fd3f7227d457cae05683a7eb46517134fc
Amanda Treutler <amandathedev@gmail.com>
2019-02-06 14:00:35 -0600

    Old guessing game version

---
+++ guessingGame.js
@@ -0,0 +1,14 @@
+var secretNumber = 4;
+
+var guess = Number(prompt("Guess a number between 1 and 10.");
+
+if(guess) === secretNumber {
+	alert("You're correct!");
+}
+
+else if(guess) > secretNumber{
+	alert("Too high. Guess again!");
+}
+else {
+	alert("Too low. Guess again!");
+}
